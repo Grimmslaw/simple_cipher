73e0e58b8c9b6f07cc39f87cb3ab77a7cfd42eb1
dwrickey <dwrickey@uab.edu>
2018-12-30 19:54:01 -0600

    removed some_crypt submodule in favor of newly added some-crypt pip install

--- simple_cipher.py
+++ simple_cipher.py
@@ -2,10 +2,35 @@
 
 # ==> STL Imports
 from importlib import import_module
+from typing import Tuple
 
 # ==> Core/Local Imports
 from args.args import parse
-from some_crypt.util.file_io import read_txt_file
+from utils.file_io import read_txt_file
+
+
+def should_do_pip_install() -> Tuple[bool, bool]:
+    print("It appears you do not have 'some_crypt' installed.")
+    yesses = ['y', 'yes', 'yessir', 'yep']
+
+    response = input("Would you like to `pip install` it? (Y/n)").lower()
+    if response in yesses:
+        return True, False
+
+    response = input("Would you like to `pip install --user` it? (Y/n)").lower()
+    if response in yesses:
+        return True, True
+
+    return False, False
+
+
+def do_pip_install(user_flag=False) -> None:
+    import sys
+    import subprocess
+    if user_flag:
+        subprocess.call([sys.executable, "-m", "pip", "install", "--user", "some-crypt"])
+    else:
+        subprocess.call([sys.executable, "-m", "pip", "install", "some-crypt"])
 
 
 def encrypt(cipher_module, target: str,
@@ -18,22 +43,38 @@ def encrypt(cipher_module, target: str,
     else:
         plaintext = target
 
-    if strip_frmt:
-        return cipher_module.encrypt(plaintext, key, strip_frmt=True)
-    return cipher_module.encrypt(plaintext, key)
+    return cipher_module.encrypt(plaintext, key, strip_frmt=strip_frmt)
 
 
-def decrypt(cipher_module, target: str, key: int or str, target_type: str) -> str:
-    # TODO:
-    pass
+def decrypt(cipher_module, target: str,
+            key: int or str, target_type: str,
+            strip_frmt=True) -> str:
+    assert target_type in ['string', 'file']
+
+    if target_type is 'file':
+        ciphertext = read_txt_file(target)
+    else:
+        ciphertext = target
+
+    return cipher_module.decrypt(ciphertext, key, strip_frmt=strip_frmt)
 
 
 def main():
     argv = parse()
     assert argv.mode in ['encrypt', 'decrypt']
 
-    cipher_module = import_module(f".{argv.cipher}", "some_crypt.ciphers")
-    res = globals()[argv.mode](cipher_module, argv.target, argv.key, argv.target_type)
+    try:
+        cipher_module = import_module(f".{argv.cipher}", "some_crypt.ciphers")
+    except ModuleNotFoundError:
+        _do_pip, _user = should_do_pip_install()
+        if _do_pip:
+            do_pip_install(_user)
+            cipher_module = import_module(f".{argv.cipher}", "some_crypt.ciphers")
+        else:
+            print("Aborting.")
+            quit()
+
+    res = globals()[argv.mode](cipher_module, argv.target, argv.key, argv.target_type, argv.strip_frmt)
 
     if argv.output is None:
         print(res)
